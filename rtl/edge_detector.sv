b4da3e7f04a1f2e0830221451815163c46ec6448
Shahid-Uddin-Ahmed-DSi <159726180+Shahid-Uddin-Ahmed-DSi@users.noreply.github.com>
2024-02-13 21:53:33 +0600

    edge_detector.sv

--- rtl/edge_detector.sv
+++ rtl/edge_detector.sv
@@ -1,23 +1,21 @@
-module edge_detector (
-
-	input logic global_rst,
-	input logic arst_req_a,
+module edge_detector(
+	input logic e_data_i,
 	input logic clk_ref,
-	output logic edge_out
+	output logic edge_out_bar
+);
 
+	logic d_2, q_bar_1;
 
-	);
-  
-	logic q1,q2;
-  
-	always_ff @ (posedge clk_ref)
-    
-	begin
-		q1<= global_rst && ~arst_req_a;
-		q2<= ~{(global_rst && ~arst_req_a) ||(~q1)};
-      
- 	end
-	
-	assign edge_out=~q2;
-  
+	always_ff @ (posedge clk_ref) begin
+		q_bar_1 <= ~(e_data_i);
+	end
+
+	always_comb begin
+      d_2 = ~(q_bar_1 | e_data_i);
+	end
+
+	always_ff @ (posedge clk_ref) begin
+		edge_out_bar <= ~d_2;
+	end
+
 endmodule
