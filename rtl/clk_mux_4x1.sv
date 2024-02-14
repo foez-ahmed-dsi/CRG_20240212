295c4a9f715976ad62b2fd4abed42755df98843d
Foez Ahmed <97046688+foez-ahmed@users.noreply.github.com>
2024-02-14 14:30:29 +0600

    Update clk_mux_4x1.sv

--- rtl/clk_mux_4x1.sv
+++ rtl/clk_mux_4x1.sv
@@ -9,12 +9,12 @@ module clk_mux_4x1(input logic [1:0] sel,
     logic clk_out_0;
     logic clk_out_1;
 
-    clk_mux_4x1.sv inst_0 (.sel(sel[0]), .PLL1(pll[0]), .PLL2(pll[1]), .CLK_I(clk_out_0));
+    clk_mux_2x1.sv inst_0 (.sel(sel[0]), .PLL1(pll[0]), .PLL2(pll[1]), .CLK_I(clk_out_0));
 
 
-    clk_mux_4x1.sv inst_1 (.sel(sel[0]), .PLL1(pll[2]), .PLL2(pll[3]), .CLK_I(clk_out_1));
+    clk_mux_2x1.sv inst_1 (.sel(sel[0]), .PLL1(pll[2]), .PLL2(pll[3]), .CLK_I(clk_out_1));
 
 
-    clk_mux_4x1.sv inst_2 (.sel(sel[1]), .PLL1(clk_out_0), .PLL2(clk_out_1), .CLK_I(clk_out));
+    clk_mux_2x1.sv inst_2 (.sel(sel[1]), .PLL1(clk_out_0), .PLL2(clk_out_1), .CLK_I(clk_out));
 
 endmodule
