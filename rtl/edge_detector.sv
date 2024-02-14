b44be5121db7d89b83f0cc66623d4234e22c2dfe
Shahid-Uddin-Ahmed-DSi <159726180+Shahid-Uddin-Ahmed-DSi@users.noreply.github.com>
2024-02-14 13:09:41 +0600

    edge_detector.sv (#19)

--- rtl/edge_detector.sv
+++ rtl/edge_detector.sv
@@ -1,21 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//////// Name: Shahid Uddin Ahmed
+//////// Email: shahid.ahmed@dsinnovators.com
+////////////////////////////////////////////////////////////////////////////////
 module edge_detector(
-	input logic e_data_i,
-	input logic clk_ref,
-	output logic edge_out_bar
+    input logic e_data_i,
+    input logic clk_ref,
+    output logic edge_out_bar
 );
+    logic d_2, q_bar_1;
 
-	logic d_2, q_bar_1;
-
-	always_ff @ (posedge clk_ref) begin
-		q_bar_1 <= ~(e_data_i);
-	end
-
-	always_comb begin
-      d_2 = ~(q_bar_1 | e_data_i);
-	end
-
-	always_ff @ (posedge clk_ref) begin
-		edge_out_bar <= ~d_2;
-	end
+    always_ff @ (posedge clk_ref) begin
+        q_bar_1 <= ~(e_data_i);
+    end
+	
+    always_comb begin
+        d_2 = ~(q_bar_1 | e_data_i);
+    end
+	
+    always_ff @ (posedge clk_ref) begin
+        edge_out_bar <= ~d_2;
+    end
 
 endmodule
