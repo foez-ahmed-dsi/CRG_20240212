601a10f0516946dd62bd63c1aca449f87c71bfb8
Foez Ahmed <97046688+foez-ahmed@users.noreply.github.com>
2024-02-14 12:31:50 +0600

    Update clock_gate.sv

--- rtl/clock_gate.sv
+++ rtl/clock_gate.sv
@@ -5,26 +5,25 @@
 //
 ////////////////////////////////////////////
 module DFF_CG(
-  input logic EN,
-  input logic CLK,
-  input logic RST_N,
-  output logic CLK_OUT
+    input logic EN,
+    input logic CLK,
+    input logic RST_N,
+    output logic CLK_OUT
 );
 
-	logic clk_inv;
-	logic Q_holder;
+    logic clk_inv;
+    logic Q_holder;
 	
-	assign clk_inv = ~CLK;
+    assign clk_inv = ~CLK;
 
-	always_ff @ (posedge clk_inv or negedge RST_N) begin
-		if (~RST_N) begin
-			Q_holder <= 0;
-		end else begin
-			Q_holder <= EN;
-		end
+    always_ff @ (posedge clk_inv or negedge RST_N) begin
+        if (~RST_N) begin
+            Q_holder <= 0;
+        end else begin
+            Q_holder <= EN;
 	end
+    end
   
-	assign CLK_OUT = Q_holder & CLK;
+    assign CLK_OUT = Q_holder & CLK;
 
 endmodule
-
