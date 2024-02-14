a1b54c4119c057fb8b27c40a7ccf1a8c11dc84e8
NazmusSakib-Punno <159714831+NazmusSakib-Punno@users.noreply.github.com>
2024-02-14 14:23:06 +0600

    Added the md file, changed the delay_gen module (#26)

---
+++ docs/delay_gen_doc.md
@@ -0,0 +1,7 @@
+#Delay Generator Circuit
+
+##Introduction:
+
+As a delay generator circuit, we used a counter here. It simply counts up to the desired delay / count using an adder whose one output being always 1. The register we used of any bit considering required limit. The block will work as a desired generator for edge detected by previous block, and will control the global reset, reset request and output clock.
+
+<img src="./delay_gen.svg>
--- rtl/delay_gen.sv
+++ rtl/delay_gen.sv
@@ -1,3 +1,7 @@
+///////////////////////////////////////////////////////////////////////////////
+//Author: Md Nazmus Sakib
+//This is a delay generator which is constructed using counter
+///////////////////////////////////////////////////////////////////////////////
 module delay_gen #(
     parameter count_range=128
 )(
@@ -8,19 +12,19 @@ module delay_gen #(
 
 	// TODO: REWRITE
     logic [$clog(n+1)-1:0] count;
-    always_comb begin 
-        en=~out;
-    end
+    logic                  en;
+    
+    assign out=(count+1==count_range);
+    assign en=~out;
+
+
 
     always_ff @(posedge clk or negedge rst) begin
         if (rst) begin
             count<= 0;
-            out  <= 0;
         end else begin
-			if(count+1 == count_range) begin
+			if(en) begin
                 count <= count + 1; 
-            end else begin
-                out <= 1;
             end
         end
     end
