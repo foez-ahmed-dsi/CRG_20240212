90af4a8c11b76d640181f9cff4d715395a5fda5d
sadmanishrak-dsi <159742673+sadmanishrak-dsi@users.noreply.github.com>
2024-02-14 14:26:11 +0600

    15 glitch free mux code (#25)
    * updating code
    
    * Update clock_gate.sv
    
    * Update clock_gate.sv
    
    * Update clock_gate.sv
    
    * Update and rename delaygen.sv to delay_gen.sv
    
    * I want to chenge these files.
    
    * commiting docs/clk-gate.svg and docs/clock_gate.md
    
    * here I hae created the module that connects the clock mux and clock gating module
    
    ---------
    
    Co-authored-by: Foez Ahmed <97046688+foez-ahmed@users.noreply.github.com>

--- rtl/delay_gen.sv
+++ rtl/delay_gen.sv
@@ -2,6 +2,7 @@
 //Author: Md Nazmus Sakib
 //This is a delay generator which is constructed using counter
 ///////////////////////////////////////////////////////////////////////////////
+
 module delay_gen #(
     parameter count_range=128
 )(
@@ -9,21 +10,18 @@ module delay_gen #(
     input  logic                  arst_n,
     output logic                  out
 );
-
-	// TODO: REWRITE
+  
     logic [$clog(n+1)-1:0] count;
     logic                  en;
     
     assign out=(count+1==count_range);
     assign en=~out;
 
-
-
     always_ff @(posedge clk or negedge rst) begin
         if (rst) begin
             count<= 0;
         end else begin
-			if(en) begin
+			      if(en) begin
                 count <= count + 1; 
             end
         end
