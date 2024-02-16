33a4b8f2f19122f95b0186261c51c05609cb8b7f
Foez Ahmed <97046688+foez-ahmed@users.noreply.github.com>
2024-02-16 16:28:04 +0600

    Update top_module.sv

--- rtl/top_module.sv
+++ rtl/top_module.sv
@@ -3,12 +3,10 @@
 // Email: sadmanishrak.work@gmail.com
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*
-M   int Number of PLL sources
-N   int Number of clock reset outputs
-*/
-
-module top_module #(parameter M = 4, parameter N = 8) (
+module top_module #(
+    parameter int M = 4, // M - int Number of PLL sources
+    parameter int N = 8  // N - int Number of clock reset outputs
+) (
 
     input logic pll_i[M],
     input logic [$clog2(M)-1:0]sel_i[N],
@@ -25,24 +23,18 @@ module top_module #(parameter M = 4, parameter N = 8) (
 // INSTANCE BLOCK
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-    genvar i;
-
-    generate
-
-        for (i = 0; i < N; i++) begin
-            sub_top_module sub_top_module_inst(
-                .pll_i(pll_i[M-1:0]),
-                .sel_i(sel_i[$clog2(M)-1:0]),
-                .glob_arst_n_i(glob_arst_n_i),
-                .arst_req_i(arst_req_i[i]),
-                .ref_clk_i(ref_clk_i),
-                .en_i(en_i[i]),
-                .clk_o(clk_o[i]),
-                .arst_n_o(arstn_o[i])
-            );
-        end
-
-    endgenerate
+    for ( genvar i = 0; i < N; i++) begin : gen_sub_top
+        sub_top_module sub_top_module_inst(
+            .pll_i(pll_i[M-1:0]),
+            .sel_i(sel_i[$clog2(M)-1:0]),
+            .glob_arst_n_i(glob_arst_n_i),
+            .arst_req_i(arst_req_i[i]),
+            .ref_clk_i(ref_clk_i),
+            .en_i(en_i[i]),
+            .clk_o(clk_o[i]),
+            .arst_n_o(arstn_o[i])
+        );
+    end
 
 endmodule
 
